e4189a9bce64af841aca04ea29bc377121d06f22
xiaojieluo <xiaojieluoff@gmail.com>
2017-05-18 12:10:26 +0800

    add sha256, sha224 encode

--- amazingtool/api/handler/EncodeHandler.py
+++ amazingtool/api/handler/EncodeHandler.py
@@ -6,15 +6,51 @@ class md5(APIHandler):
     md5 加密
     '''
     def get(self):
-        query = self.get_query_argument('query', '')
-        result = hashlib.md5(query.encode('utf-8')).hexdigest()
+        text = self.get_query_argument('text', '')
+        result = hashlib.md5(text.encode('utf-8')).hexdigest()
 
         data = {
-            'query': query,
+            'text': text,
             'result': result
         }
 
-        if self.db.md5.find_one({'query':query}) is None:
-            self.db.md5.insert_one({'query':query,'result':result})
+        if self.db.md5.find_one({'text':text}) is None:
+            self.db.md5.insert_one({'text':text,'result':result})
+
+        self.write_json(data)
+
+class sha256(APIHandler):
+    '''
+    sha256 加密
+    '''
+    def get(self):
+        text = self.get_query_argument('text', '')
+        result = hashlib.sha256(text.encode('utf-8')).hexdigest()
+
+        data = dict(
+            text = text,
+            result = result
+        )
+
+        if self.db.sha256.find_one({'text':text}) is None:
+            self.db.sha256.insert_one({'text':text,'result':result})
+
+        self.write_json(data)
+
+class sha224(APIHandler):
+    '''
+    sha224 加密
+    '''
+    def get(self):
+        text = self.get_query_argument('text', '')
+        result = hashlib.sha224(text.encode('utf-8')).hexdigest()
+
+        data = dict(
+            text = text,
+            result = result
+        )
+
+        if self.db.sha224.find_one({'text':text}) is None:
+            self.db.sha224.insert_one({'text':text, 'result':result})
 
         self.write_json(data)
--- amazingtool/api/route.py
+++ amazingtool/api/route.py
@@ -8,4 +8,6 @@ route = [
     (r'/', Index.index),
     (r'/user/(.*)', User.index),
     (r'/encode/md5', Encode.md5),
+    (r'/encode/sha256', Encode.sha256),
+    (r'/encode/sha224', Encode.sha224),
 ]
