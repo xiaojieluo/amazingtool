addf76945bef49a40b0909ee44c0d655cbddfe55
xiaojieluo <xiaojieluoff@gmail.com>
2017-06-15 17:17:08 +0800

    modify

--- amazingtool/api/handler/QueryHandler.py
+++ amazingtool/api/handler/QueryHandler.py
@@ -7,15 +7,6 @@ import asyncio
 import requests
 import tornado
 from copy import deepcopy
-# import logging
-# # 通过下面的方式进行简单配置输出方式与日志级别
-# logging.basicConfig(filename='logger.log', level=logging.INFO)
-#
-# logging.debug('debug message')
-# logging.info('info message')
-# logging.warn('warn message')
-# logging.error('error message')
-# logging.critical('critical message')
 
 class index(APIHandler):
     def get(self):
--- amazingtool/api/handler/__init__.py
+++ amazingtool/api/handler/__init__.py
@@ -1,4 +1,1 @@
-import sys
-sys.path.append('../')
 
-from amazingtool.db import db
